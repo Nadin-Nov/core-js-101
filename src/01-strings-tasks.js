ed015546e8688d74b34fa16c3278d90cdcc38d4f
Nadin Novoselova <nadja.novoselova@gmail.com>
2024-08-27 18:26:58 +0300

    fix: change end of string lf

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -115,8 +115,9 @@ function removeLeadingAndTrailingWhitespaces(string) {
  *   'A', 5  => 'AAAAA'
  *   'cat', 3 => 'catcatcat'
  */
-function repeatString(/* value, count */) {
-  throw new Error('Not implemented');
+function repeatString(str, n) {
+  const num = Number(n)
+  let result = str.repeat(num);
 }
 
 /**
@@ -131,8 +132,9 @@ function repeatString(/* value, count */) {
  *   'I like legends', 'end' => 'I like legs',
  *   'ABABAB','BA' => 'ABAB'
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  let result = str.replace(value, "");
+  return result;
 }
 
 /**
@@ -146,8 +148,9 @@ function removeFirstOccurrences(/* str, value */) {
  *   '<span>' => 'span'
  *   '<a>' => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(tag) {
+  let result = tag.slice(1, -1);
+  return result;
 }
 
 
@@ -161,8 +164,9 @@ function unbracketTag(/* str */) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
+function convertToUpperCase(str) {
+  const  newStr = str.toUpperCase();
+  return newStr;
 }
 
 /**
